097f3f0ace809bcabd5983496384738e7622c0e8
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-04-17 17:38:13 +0200

    aggregate and save lifetime data

---
+++ lifetime_input_output/lifetime_script.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+from posted.noslag import DataSet
+
+def get_git_root():
+    """Returns the absolute path of the Git repository root."""
+    return subprocess.run(
+        ["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True
+    ).stdout.strip()
+
+teds = DataSet('Buildings and Infrastructure Lifetime')
+x = teds.aggregate(period=2025)
+
+git_root = os.path.normpath(get_git_root())
+git_root_parent = os.path.dirname(git_root)
+
+output_path = os.path.join(git_root_parent,
+                           "madrat_wd", 
+                           "sources",
+                           "PostedLifetimes",
+                           "v1",
+                           "buildings_and_infrastructure_lifetime.csv")
+x.to_csv(output_path, index=False)
