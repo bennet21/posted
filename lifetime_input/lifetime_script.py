2a776c96499552ffb70b9735f932dc6bd17b2cd4
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-05-06 18:12:48 +0200

    refactor masks: used sources are now defined, instead of unused as before.

--- lifetime_input/lifetime_script.py
+++ lifetime_input/lifetime_script.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
+import pandas as pd
 
-from posted.noslag import DataSet
+from posted.noslag import DataSet, Mask
 
 def get_git_root():
     """Returns the absolute path of the Git repository root."""
@@ -9,8 +10,34 @@ def get_git_root():
         ["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True
     ).stdout.strip()
 
+masks1 = [
+    Mask(where={'region': 'Africa'}, use={'source': 'Hatayama10'}),
+    Mask(where={'region': 'CIS'}, use={'source': 'Hatayama10'}),
+    Mask(where={'region': 'Europe'}, use={'source': 'Cao17'}),
+    Mask(where={'region': 'India'}, use={'source': 'Cao21'}),
+    Mask(where={'region': 'Iran'}, use={'source': 'Hosseinijou21'}),
+    Mask(where={'region': 'Japan', 'end_use': 'Res'}, use={'source': 'Deetman20'}),
+    Mask(where={'region': 'Japan', 'end_use': 'NonRes'}, use={'source': 'Cao17'}),
+    Mask(where={'region': 'Japan', 'end_use': 'Civ'}, use={'source': 'Cao17'}),
+    Mask(where={'region': 'USA'}, use={'source': 'Kapur08'})
+]
+
+masks2 = [
+    Mask(where={'region': 'China', 'end_use': 'Civ'}, use={'source': 'Cao17'}),
+    Mask(where={'region': 'China', 'end_use': 'Res'}, use={'source': 'Cao19'}),
+]
+
+masks3 = [
+    # South america aggregation causes issues with aggregation as it is only one value.
+    # As a workaround, for now we don't specify use_case, even though only Res is available.
+    Mask(where={'region': 'Rest of South America'}, use={'source': 'Deetman20'}),
+]
+
 teds = DataSet('Buildings and Infrastructure Lifetime')
-x = teds.aggregate(period=2025)
+x = pd.concat([
+    teds.aggregate(period=2025, region=["Africa", "CIS", "Europe", "India", "Iran", "Japan", "USA", "Rest of South America"], masks=masks1),
+    teds.aggregate(period=2025, region="China", end_use=["Res", "Civ"], masks=masks2),
+]).sort_values(by=['end_use', 'time_range', 'region']).reset_index(drop=True)
 
 git_root = os.path.normpath(get_git_root())
 git_root_parent = os.path.dirname(git_root)
@@ -18,7 +45,7 @@ git_root_parent = os.path.dirname(git_root)
 madrat_output_path = os.path.join(git_root_parent,
                            "madrat_wd", 
                            "sources",
-                           "PostedLifetimes",
+                           "PostedBuiltLifespan",
                            "v1",
                            "buildings_and_infrastructure_lifetime.csv")
 local_output_path = os.path.join("lifetime_input",
