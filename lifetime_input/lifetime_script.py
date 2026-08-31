7d37253a2b863994fadf2cd1f34078887e8c2051
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2026-08-31 18:03:18 +0200

    update lifetimes

--- lifetime_input/lifetime_script.py
+++ lifetime_input/lifetime_script.py
@@ -10,33 +10,15 @@ def get_git_root():
         ["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True
     ).stdout.strip()
 
-masks1 = [
-    Mask(where={'region': 'Africa'}, use={'source': 'Hatayama10'}),
-    Mask(where={'region': 'CIS'}, use={'source': 'Hatayama10'}),
-    Mask(where={'region': 'Europe'}, use={'source': 'Cao17'}),
-    Mask(where={'region': 'India'}, use={'source': 'Cao21'}),
-    Mask(where={'region': 'Iran'}, use={'source': 'Hosseinijou21'}),
-    Mask(where={'region': 'Japan', 'end_use': 'Res'}, use={'source': 'Deetman20'}),
-    Mask(where={'region': 'Japan', 'end_use': 'NonRes'}, use={'source': 'Cao17'}),
-    Mask(where={'region': 'Japan', 'end_use': 'Civ'}, use={'source': 'Cao17'}),
-    Mask(where={'region': 'USA'}, use={'source': 'Kapur08'})
-]
-
-masks2 = [
-    Mask(where={'region': 'China', 'end_use': 'Civ'}, use={'source': 'Cao17'}),
-    Mask(where={'region': 'China', 'end_use': 'Res'}, use={'source': 'Cao19'}),
-]
-
-masks3 = [
-    # South america aggregation causes issues with aggregation as it is only one value.
-    # As a workaround, for now we don't specify use_case, even though only Res is available.
-    Mask(where={'region': 'Rest of South America'}, use={'source': 'Deetman20'}),
+masks = [
+    Mask(use = {'source': 'Buyle13'}, weight=float('nan'), other=1.0, comment = "case studies"),
+    Mask(where={'region': 'China', 'end_use': 'Res'}, use={'source': 'Cao19'}, comment = "time-dependent data"),
 ]
 
 teds = DataSet('Buildings and Infrastructure Lifetime')
+# make sure there are no duplicate entries from one source; otherwise those will be summed.
 x = pd.concat([
-    teds.aggregate(period=2025, region=["Africa", "CIS", "Europe", "India", "Iran", "Japan", "USA", "Rest of South America"], masks=masks1),
-    teds.aggregate(period=2025, region="China", end_use=["Res", "Civ"], masks=masks2),
+    teds.aggregate(period=2025, masks=masks),
 ]).sort_values(by=['end_use', 'time_range', 'region']).reset_index(drop=True)
 
 git_root = os.path.normpath(get_git_root())
@@ -46,7 +28,7 @@ madrat_output_path = os.path.join(git_root_parent,
                            "madrat_wd", 
                            "sources",
                            "PostedBuiltLifespan",
-                           "v1",
+                           "v20260831",
                            "buildings_and_infrastructure_lifetime.csv")
 local_output_path = os.path.join("lifetime_input",
                                  "output",
